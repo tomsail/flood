d64e67f833bcc5436ee173be20fd452c812bf103
thomas saille <saillour.thomas@gmail.com>
2021-04-14 18:30:42 +0200

    biarritz

--- main.1f19ae8e.js
+++ main.1f19ae8e.js
@@ -97387,7 +97387,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "35023" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39261" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
